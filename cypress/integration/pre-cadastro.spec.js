997af02ac5ac20aa6297a81dd6dad725e608fc3c
Paulo Victor <p.paulo.suh@gmail.com>
2022-01-31 12:59:05 -0300

    usando Custom Commands

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -24,5 +24,10 @@ describe('Funcionalidade Pré Cadastro', () => {
         cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
     });
 
+    it.only('Deve completar o pré-cadastro com sucesso usando Comandos Customizados', () => {
+        let emailFaker2 = faker.internet.email()
+        cy.preCadastro(emailFaker2, 'senha!@#forte', 'Paulo', 'Victor')
+        cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
+    });
 
 });
--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -20,13 +20,21 @@ describe('Funcionalidade Página de produtos', () => {
 
         cy.get('[class="product-block grid"]')
             .contains('Ariel Roll Sleeve Sweatshirt').click()
-            cy.get('.button-variable-item-M').click()
-            cy.get('.button-variable-item-Green').click()
-            cy.get('.input-text').clear().type(quantidade)
-            cy.get('.single_add_to_cart_button').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
 
-            cy.get('.dropdown-toggle > .mini-cart-items').should('contain',quantidade)
-            cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt”')
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt”')
 
-    })
+    });
+
+    it('Deve adicionar produtos ao carrinho usando comando customizado', () => {
+        cy.addProdutos('Aero Daily Fitness Tee', 'XS', 'Yellow', 2)
+    });
+
+    it('Deve adicionar produtos ao carrinho usando comando customizado', () => {
+        cy.addProdutos('Ariel Roll Sleeve Sweatshirt', 'S', 'Green', 1)
+    });
 })
